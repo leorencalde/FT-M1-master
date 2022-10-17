7c5ee1505cfe2b6aeee682fc30b19e1b32aa2076
unknown <leo.rencalderon@gmail.com>
2022-10-17 14:04:09 -0500

    homework 7

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -5,8 +5,18 @@ function factorear(num) {
   // Factorear el número recibido como parámetro y devolver en un array
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
-  // Tu código:
-
+  // Tu código:          
+  let array = [1];
+  let i = 2;
+  while (num !== 1){
+    if (num % i === 0){
+      array.push(i);
+      num = num/i;
+    }else{
+      i++;
+    }
+  }
+  return array;
 }
 
 function bubbleSort(array) {
@@ -14,7 +24,19 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let swap = true;
+  while (swap){
+    swap = false;
+    for (let i = 0; i < array.length-1; i++) {
+      if (array[i] > array[i+1]){
+        let aux = array[i];
+        array[i] = array[i+1];
+        array[i+1] = aux;
+        swap = true;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +45,16 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 1; i < array.length; i++) {
+    let j = i-1;
+    let aux = array[i];
+    while(j>=0 && aux < array[j]){
+      array[j+1]=array[j];
+      j--;
+    }
+    array[j+1] = aux;
+  }
+  return array;
 }
 
 
@@ -32,7 +63,20 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length-1; i++) {
+    let min = i;
+    for (let j = i+1; j < array.length; j++) {
+      if(array[min] > array[j]){
+        min = j;
+      }
+    }
+    if (i !== min){
+      let aux = array[i];
+      array[i] =  array[min];
+      array[min] = aux;
+    }
+  }
+  return array;
 }
 
 
